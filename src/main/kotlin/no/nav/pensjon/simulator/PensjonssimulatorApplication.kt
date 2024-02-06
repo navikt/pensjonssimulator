63d10a6096ad76df2aa8d441c9f03f42db169c5c
Espen Gjøstøl <espen.oskar.gjostol@nav.no>
2024-02-06 13:24:33 +0100

    Use Spring Boot

--- src/main/kotlin/no/nav/pensjon/simulator/PensjonssimulatorApplication.kt
+++ src/main/kotlin/no/nav/pensjon/simulator/PensjonssimulatorApplication.kt
@@ -1,4 +1,12 @@
 package no.nav.pensjon.simulator
 
-fun main() {
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration
+import org.springframework.boot.runApplication
+
+@SpringBootApplication(exclude = [UserDetailsServiceAutoConfiguration::class])
+class PensjonssimulatorApplication
+
+fun main(args: Array<String>) {
+    runApplication<PensjonssimulatorApplication>(*args)
 }
--- src/test/kotlin/no/nav/pensjon/simulator/PensjonssimulatorApplicationTest.kt
+++ src/test/kotlin/no/nav/pensjon/simulator/PensjonssimulatorApplicationTest.kt
@@ -1,7 +1,11 @@
 package no.nav.pensjon.simulator
 
 import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
+@SpringBootTest
+@ActiveProfiles("test")
 class PensjonssimulatorApplicationTest {
 
     @Test

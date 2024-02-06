a270b21a5d47002137d7087693074ee102ac7e63
Espen Gjøstøl <espen.oskar.gjostol@nav.no>
2024-02-06 14:34:20 +0100

    Configure Spring Security
    (require JWT from Entra ID preprod)

---
+++ src/main/kotlin/no/nav/pensjon/simulator/tech/security/SecurityConfiguration.kt
@@ -0,0 +1,23 @@
+package no.nav.pensjon.simulator.tech.security
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.security.config.Customizer
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+class SecurityConfiguration {
+
+    @Bean
+    fun filterChain(http: HttpSecurity): SecurityFilterChain =
+        http
+            .authorizeHttpRequests {
+                it
+                    .requestMatchers(HttpMethod.GET, "/internal/**", "/error").permitAll()
+                    .anyRequest().authenticated()
+            }
+            .oauth2ResourceServer { it.jwt(Customizer.withDefaults()) }
+            .build()
+}

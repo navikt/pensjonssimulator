7ddc8496aecdaee777bd684503a50c5626026e44
Espen Gjøstøl <50329374+espengjostol@users.noreply.github.com>
2024-10-22 10:46:17 +0200

    PEK-555 Add support for client credentials tokens (#34)
    (issued by Entra ID to PEN and PKB)

---
+++ src/test/kotlin/no/nav/pensjon/simulator/tech/security/ApiAuthenticationManagerResolverTest.kt
@@ -0,0 +1,62 @@
+package no.nav.pensjon.simulator.tech.security
+
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+import jakarta.servlet.http.HttpServletRequest
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.`when`
+import org.springframework.security.authentication.ProviderManager
+import org.springframework.security.core.Authentication
+
+class ApiAuthenticationManagerResolverTest : FunSpec({
+
+    test("resolve uses Entra ID when path is /api/nav/**") {
+        val (entraProvider, entraAuthentication) = arrangeAuth(isAuthenticated = true)
+        val (maskinportenProvider, _) = arrangeAuth(isAuthenticated = false)
+        val request = arrangeRequest(path = "/api/nav/service1")
+        val resolver = ApiAuthenticationManagerResolver(entraProvider, maskinportenProvider)
+
+        val authenticationManager = resolver.resolve(request)
+
+        authenticationManager.authenticate(entraAuthentication).isAuthenticated shouldBe true
+    }
+
+    test("resolve uses Entra ID when path is /api/tpo/**") {
+        val (entraProvider, entraAuthentication) = arrangeAuth(isAuthenticated = true)
+        val (maskinportenProvider, _) = arrangeAuth(isAuthenticated = false)
+        val request = arrangeRequest(path = "/api/tpo/service1")
+        val resolver = ApiAuthenticationManagerResolver(entraProvider, maskinportenProvider)
+
+        val authenticationManager = resolver.resolve(request)
+
+        authenticationManager.authenticate(entraAuthentication).isAuthenticated shouldBe true
+    }
+
+    test("resolve uses Maskinporten when path is /api/v4/simuler-alderspensjon") {
+        val (entraProvider, _) = arrangeAuth(isAuthenticated = false)
+        val (maskinportenProvider, maskinportenAuthentication) = arrangeAuth(isAuthenticated = true)
+        val request = arrangeRequest(path = "/api/v4/simuler-alderspensjon")
+        val resolver = ApiAuthenticationManagerResolver(entraProvider, maskinportenProvider)
+
+        val authenticationManager = resolver.resolve(request)
+
+        authenticationManager.authenticate(maskinportenAuthentication).isAuthenticated shouldBe true
+    }
+})
+
+private fun arrangeAuth(isAuthenticated: Boolean): Pair<ProviderManager, Authentication> {
+    val authentication = mock(Authentication::class.java).also {
+        `when`(it.isAuthenticated).thenReturn(isAuthenticated)
+    }
+
+    val provider = mock(ProviderManager::class.java).also {
+        `when`(it.authenticate(authentication)).thenReturn(authentication)
+    }
+
+    return Pair(provider, authentication)
+}
+
+private fun arrangeRequest(path: String): HttpServletRequest =
+    mock(HttpServletRequest::class.java).also {
+        `when`(it.servletPath).thenReturn(path)
+    }

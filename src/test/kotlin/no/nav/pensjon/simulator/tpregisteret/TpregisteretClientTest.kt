75aba13e39106419d1f898679524f9e49540c611
Espen Gjøstøl <espen.oskar.gjostol@nav.no>
2024-10-22 15:51:49 +0200

    Add PID test to TpregisteretClientTest

--- src/test/kotlin/no/nav/pensjon/simulator/tpregisteret/TpregisteretClientTest.kt
+++ src/test/kotlin/no/nav/pensjon/simulator/tpregisteret/TpregisteretClientTest.kt
@@ -10,9 +10,11 @@ import no.nav.pensjon.simulator.testutil.TestObjects.organisasjonsnummer
 import no.nav.pensjon.simulator.testutil.TestObjects.pid
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
+import okhttp3.mockwebserver.RecordedRequest
 import org.mockito.Mockito.mock
 import org.springframework.boot.autoconfigure.AutoConfigurations
 import org.springframework.boot.autoconfigure.web.reactive.function.client.WebClientAutoConfiguration
+import org.springframework.boot.test.context.assertj.AssertableApplicationContext
 import org.springframework.boot.test.context.runner.ApplicationContextRunner
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
@@ -28,104 +30,107 @@ class TpregisteretClientTest : FunSpec({
     beforeSpec {
         SecurityContextHolder.setContext(SecurityContextHolder.createEmptyContext())
 
-        SecurityContextHolder.getContext().authentication = EnrichedAuthentication(
-            TestingAuthenticationToken("TEST_USER", jwt),
-            EgressTokenSuppliersByService(mapOf())
-        )
+        SecurityContextHolder.getContext().authentication =
+            EnrichedAuthentication(
+                initialAuth = TestingAuthenticationToken("TEST_USER", jwt),
+                egressTokenSuppliersByService = EgressTokenSuppliersByService(mapOf())
+            )
 
-        server = MockWebServer().also { it.start() }
-        baseUrl = "http://localhost:${server!!.port}"
+        server = MockWebServer().also {
+            it.start()
+            baseUrl = "http://localhost:${it.port}"
+        }
     }
 
     afterSpec {
         server?.shutdown()
     }
 
+    test("should have PID as request body") {
+        arrangeForhold(server, harForhold = true)
+
+        arrangeContextRunner().run {
+            val client = client(baseUrl, arrangeWebClient(it))
+            client.hentErBrukerTilknyttetTpLeverandoer(pid, organisasjonsnummer)
+            server?.takeRequest()?.let(::assertBodyIsPid)
+        }
+    }
+
     test("should return true when response has forhold as true") {
+        arrangeForhold(server, harForhold = true)
 
-        val contextRunner = ApplicationContextRunner().withConfiguration(
-            AutoConfigurations.of(WebClientAutoConfiguration::class.java)
-        )
-        // Arrange
-        server!!.enqueue(
-            MockResponse()
-                .addHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-                .setResponseCode(HttpStatus.OK.value())
-                .setBody("{\"forhold\": true}")
-        )
-
-        contextRunner.run {
-            val webClientBuilder = it.getBean(WebClient.Builder::class.java)
-            val client = TpregisteretClient(baseUrl!!, "2", webClientBuilder, mock(TraceAid::class.java))
-
-            val result = client.hentErBrukerTilknyttetTpLeverandoer(pid, organisasjonsnummer)
-
-            result shouldBe true
+        arrangeContextRunner().run {
+            val client = client(baseUrl, arrangeWebClient(it))
+            client.hentErBrukerTilknyttetTpLeverandoer(pid, organisasjonsnummer) shouldBe true
         }
     }
 
     test("should return false when response has forhold as false") {
-        val contextRunner = ApplicationContextRunner().withConfiguration(
-            AutoConfigurations.of(WebClientAutoConfiguration::class.java)
-        )
-        // Arrange
-        server!!.enqueue(
-            MockResponse()
-                .addHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-                .setResponseCode(HttpStatus.OK.value())
-                .setBody("{\"forhold\": false}")
-        )
+        arrangeForhold(server, harForhold = false)
 
-        contextRunner.run {
-            val webClientBuilder = it.getBean(WebClient.Builder::class.java)
-            val client = TpregisteretClient(baseUrl!!, "2", webClientBuilder, mock(TraceAid::class.java))
-
-            val result = client.hentErBrukerTilknyttetTpLeverandoer(pid, organisasjonsnummer)
-
-            result shouldBe false
+        arrangeContextRunner().run {
+            val client = client(baseUrl, arrangeWebClient(it))
+            client.hentErBrukerTilknyttetTpLeverandoer(pid, organisasjonsnummer) shouldBe false
         }
     }
 
     test("should return false when response is null") {
-        val contextRunner = ApplicationContextRunner().withConfiguration(
-            AutoConfigurations.of(WebClientAutoConfiguration::class.java)
-        )
-        // Arrange
-        server!!.enqueue(
-            MockResponse()
-                .addHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-                .setResponseCode(HttpStatus.OK.value())
-                .setBody("{\"forhold\": null}")
-        )
+        arrangeForhold(server, harForhold = null)
 
-        contextRunner.run {
-            val webClientBuilder = it.getBean(WebClient.Builder::class.java)
-            val client = TpregisteretClient(baseUrl!!, "2", webClientBuilder, mock(TraceAid::class.java))
-
-            val result = client.hentErBrukerTilknyttetTpLeverandoer(pid, organisasjonsnummer)
-
-            result shouldBe false
+        arrangeContextRunner().run {
+            val client = client(baseUrl, arrangeWebClient(it))
+            client.hentErBrukerTilknyttetTpLeverandoer(pid, organisasjonsnummer) shouldBe false
         }
     }
 
-    test("should return false when tpregisteret returns 500") {
-        val contextRunner = ApplicationContextRunner().withConfiguration(
-            AutoConfigurations.of(WebClientAutoConfiguration::class.java)
-        )
-        // Arrange
-        server!!.enqueue(
-            MockResponse()
-                .addHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-                .setResponseCode(HttpStatus.INTERNAL_SERVER_ERROR.value())
-        )
+    test("should return false when tpregisteret has internal server error") {
+        arrangeInternalError(server)
 
-        contextRunner.run {
-            val webClientBuilder = it.getBean(WebClient.Builder::class.java)
-            val client = TpregisteretClient(baseUrl!!, "2", webClientBuilder, mock(TraceAid::class.java))
-
-            val result = client.hentErBrukerTilknyttetTpLeverandoer(pid, organisasjonsnummer)
-
-            result shouldBe false
+        arrangeContextRunner().run {
+            val client = client(baseUrl, arrangeWebClient(it))
+            client.hentErBrukerTilknyttetTpLeverandoer(pid, organisasjonsnummer) shouldBe false
         }
     }
 })
+
+private fun client(baseUrl: String?, webClientBuilder: WebClient.Builder) =
+    TpregisteretClient(
+        baseUrl = baseUrl ?: throw RuntimeException("baseUrl is null"),
+        retryAttempts = "0",
+        webClientBuilder,
+        traceAid = mock(TraceAid::class.java)
+    )
+
+private fun arrangeContextRunner() =
+    ApplicationContextRunner().withConfiguration(
+        AutoConfigurations.of(WebClientAutoConfiguration::class.java)
+    )
+
+private fun arrangeWebClient(context: AssertableApplicationContext): WebClient.Builder =
+    context.getBean(WebClient.Builder::class.java)
+
+private fun arrangeForhold(server: MockWebServer?, harForhold: Boolean?) {
+    server?.enqueue(
+        MockResponse()
+            .addHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+            .setResponseCode(HttpStatus.OK.value())
+            .setBody("{\"forhold\": $harForhold}")
+    )
+}
+
+private fun arrangeInternalError(server: MockWebServer?) {
+    server?.enqueue(
+        MockResponse()
+            .addHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+            .setResponseCode(HttpStatus.INTERNAL_SERVER_ERROR.value())
+    )
+}
+
+private fun assertBodyIsPid(request: RecordedRequest) {
+    val body = request.body
+
+    ByteArray(body.size.toInt()).also {
+        body.read(it)
+        String(it) shouldBe "12345678910"
+    }
+}

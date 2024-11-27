b99530981e4451a9eea9550c28bd9ed8a04ab41f
Espen Gjøstøl <espen.oskar.gjostol@nav.no>
2024-11-27 19:08:41 +0100

    Update SISTE_GYLDIGE_OPPTJENING_AAR
    following update in PEN 2024-11-27

--- src/main/kotlin/no/nav/pensjon/simulator/core/beholdning/BeholdningUtil.kt
+++ src/main/kotlin/no/nav/pensjon/simulator/core/beholdning/BeholdningUtil.kt
@@ -5,7 +5,7 @@ import no.nav.pensjon.simulator.core.domain.regler.grunnlag.Pensjonsbeholdning
 
 object BeholdningUtil {
     // Updated annually. In PEN stored in T_APPL_PARAMETER; context.getSisteGyldigeOpptjeningsAr()
-    const val SISTE_GYLDIGE_OPPTJENING_AAR = 2022
+    const val SISTE_GYLDIGE_OPPTJENING_AAR = 2023 // valid until November 2025
 
     // TypedInformationListeUtils.findElementOfType
     fun findElementOfType(list: List<Beholdning>, type: BeholdningType) =

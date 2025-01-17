a59cb12ac3838051d71342a4be3c882d99f9694c
Espen Gjøstøl <espen.oskar.gjostol@nav.no>
2025-01-17 12:59:39 +0100

    Set ytelsekomponentTypeEnum in AfpLivsvarig

--- src/main/kotlin/no/nav/pensjon/simulator/core/domain/regler/beregning2011/AfpLivsvarig.kt
+++ src/main/kotlin/no/nav/pensjon/simulator/core/domain/regler/beregning2011/AfpLivsvarig.kt
@@ -6,31 +6,33 @@ import no.nav.pensjon.simulator.core.domain.regler.beregning.Ytelseskomponent
 import no.nav.pensjon.simulator.core.domain.regler.enum.FormelKodeEnum
 import no.nav.pensjon.simulator.core.domain.regler.enum.YtelseskomponentTypeEnum
 
+// pensjon-regler-api: no/nav/pensjon/regler/domain/beregning2011/AfpLivsvarig.kt
 @JsonSubTypes(
     JsonSubTypes.Type(value = FremskrevetAfpLivsvarig::class)
 )
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
 open class AfpLivsvarig : Ytelseskomponent {
-
     var justeringsbelop = 0
     var afpProsentgrad = 0.0
     var afpForholdstall = 0.0
 
-    override lateinit var ytelsekomponentTypeEnum: YtelseskomponentTypeEnum
+    //override var ytelsekomponentType: YtelsekomponentTypeCti = YtelsekomponentTypeCti("AFP_LIVSVARIG") // SIMDOM-EDIT
+    override var ytelsekomponentTypeEnum: YtelseskomponentTypeEnum = YtelseskomponentTypeEnum.AFP_LIVSVARIG
 
     constructor() {
+        //formelKode = FormelKodeCti("AFPx") // SIMDOM-EDIT
         formelKodeEnum = FormelKodeEnum.AFPx
     }
 
+    // SIMDOM-ADD:
     constructor(
-        //ytelsekomponentType: YtelseskomponentTypeEnum,
         formelKode: FormelKodeEnum,
         bruttoPerAr: Double = 0.0
     ) {
-        //this.ytelsekomponentTypeEnum = ytelsekomponentType
         this.formelKodeEnum = formelKode
         this.bruttoPerAr = bruttoPerAr
     }
+    // end SIMDOM-ADD
 
     constructor(source: AfpLivsvarig) : super(source) {
         afpForholdstall = source.afpForholdstall

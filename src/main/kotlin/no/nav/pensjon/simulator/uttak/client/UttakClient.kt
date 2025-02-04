07dc473f46055e641a1b5530a212efb5b817f69c
Espen Gjøstøl <espen.oskar.gjostol@nav.no>
2025-02-04 11:48:58 +0100

    PEK-981 Delete client for PEN TPO TMU service

--- src/main/kotlin/no/nav/pensjon/simulator/uttak/client/UttakClient.kt
+++
@@ -1,8 +0,0 @@
-package no.nav.pensjon.simulator.uttak.client
-
-import no.nav.pensjon.simulator.uttak.TidligstMuligUttak
-import no.nav.pensjon.simulator.uttak.TidligstMuligUttakSpec
-
-interface UttakClient {
-    fun finnTidligstMuligUttak(spec: TidligstMuligUttakSpec): TidligstMuligUttak
-}

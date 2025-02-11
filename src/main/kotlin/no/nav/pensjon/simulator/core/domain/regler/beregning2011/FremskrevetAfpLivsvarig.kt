96eced9cb62e5d6ecb0d240f71064ac8a9739762
Espen Gjøstøl <espen.oskar.gjostol@nav.no>
2025-02-11 22:28:27 +0100

    Fix 'copy constructor' for FremskrevetAfpLivsvarig

--- src/main/kotlin/no/nav/pensjon/simulator/core/domain/regler/beregning2011/FremskrevetAfpLivsvarig.kt
+++ src/main/kotlin/no/nav/pensjon/simulator/core/domain/regler/beregning2011/FremskrevetAfpLivsvarig.kt
@@ -2,11 +2,25 @@ package no.nav.pensjon.simulator.core.domain.regler.beregning2011
 
 import no.nav.pensjon.simulator.core.domain.regler.enum.YtelseskomponentTypeEnum
 
-class FremskrevetAfpLivsvarig : AfpLivsvarig(), Regulering {
+class FremskrevetAfpLivsvarig : AfpLivsvarig, Regulering {
 
     override var reguleringsfaktor = 0.0
     override var gap = 0
     var gjennomsnittligUttaksgradSisteAr = 0.0
 
     override var ytelsekomponentTypeEnum: YtelseskomponentTypeEnum = YtelseskomponentTypeEnum.FREM_AFP_LIVSVARIG
+
+    // SIMDOM-ADD
+    constructor() : super()
+
+    /**
+     * Used via reflection in PensjonUnderUtbetaling: constructor.newInstance(komponent)
+     */
+    constructor(source: FremskrevetAfpLivsvarig) : super(source) {
+        ytelsekomponentTypeEnum = YtelseskomponentTypeEnum.FREM_AFP_LIVSVARIG
+        reguleringsfaktor = source.reguleringsfaktor
+        gap = source.gap
+        gjennomsnittligUttaksgradSisteAr = source.gjennomsnittligUttaksgradSisteAr
+    }
+    // end SIMDOM-ADD
 }

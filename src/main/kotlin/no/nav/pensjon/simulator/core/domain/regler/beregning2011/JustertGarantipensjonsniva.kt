c256306bebe67239f117611bdc5116ef1123df77
Espen Gjøstøl <espen.oskar.gjostol@nav.no>
2025-03-11 12:59:20 +0100

    PEK-648 Use explicit mapping in copy constructors
    (instead of using reflection)

--- src/main/kotlin/no/nav/pensjon/simulator/core/domain/regler/beregning2011/JustertGarantipensjonsniva.kt
+++ src/main/kotlin/no/nav/pensjon/simulator/core/domain/regler/beregning2011/JustertGarantipensjonsniva.kt
@@ -1,8 +1,7 @@
 package no.nav.pensjon.simulator.core.domain.regler.beregning2011
 
-import java.io.Serializable
-
-class JustertGarantipensjonsniva : Serializable {
+// 2025-03-11
+class JustertGarantipensjonsniva {
     var garantipensjonsniva: Garantipensjonsniva? = null
     var justeringsInformasjon: JusteringsInformasjon? = null
     var belop = 0.0
--- src/main/kotlin/no/nav/pensjon/simulator/core/domain/reglerextend/beregning2011/Beregning2011Extension.kt
+++ src/main/kotlin/no/nav/pensjon/simulator/core/domain/reglerextend/beregning2011/Beregning2011Extension.kt
@@ -2,24 +2,24 @@ package no.nav.pensjon.simulator.core.domain.reglerextend.beregning2011
 
 import no.nav.pensjon.simulator.core.domain.regler.Merknad
 import no.nav.pensjon.simulator.core.domain.regler.PenPerson
+import no.nav.pensjon.simulator.core.domain.regler.beregning.BeregningRelasjon
 import no.nav.pensjon.simulator.core.domain.regler.beregning.Sluttpoengtall
-import no.nav.pensjon.simulator.core.domain.regler.beregning2011.AbstraktAvkortingsinformasjon
-import no.nav.pensjon.simulator.core.domain.regler.beregning2011.AbstraktBeregningsResultat
-import no.nav.pensjon.simulator.core.domain.regler.beregning2011.AldersberegningKapittel19
-import no.nav.pensjon.simulator.core.domain.regler.beregning2011.AvkortingsinformasjonBT
-import no.nav.pensjon.simulator.core.domain.regler.beregning2011.AvkortingsinformasjonUT
-import no.nav.pensjon.simulator.core.domain.regler.beregning2011.BeregningsInformasjon
-import no.nav.pensjon.simulator.core.domain.regler.beregning2011.BeregningsResultatAlderspensjon2011
-import no.nav.pensjon.simulator.core.domain.regler.beregning2011.FremtidigBarnetilleggperiode
-import no.nav.pensjon.simulator.core.domain.regler.beregning2011.LonnsvekstInformasjon
-import no.nav.pensjon.simulator.core.domain.regler.beregning2011.PensjonUnderUtbetaling
-import no.nav.pensjon.simulator.core.domain.regler.beregning2011.TapendeBeregningsmetode
-import no.nav.pensjon.simulator.core.domain.regler.beregning2011.TidligereBarnetilleggperiode
-import no.nav.pensjon.simulator.core.domain.regler.enum.Beregningsarsak
-import no.nav.pensjon.simulator.core.domain.regler.enum.BorMedTypeEnum
-import no.nav.pensjon.simulator.core.domain.regler.enum.SivilstandEnum
+import no.nav.pensjon.simulator.core.domain.regler.beregning2011.*
+import no.nav.pensjon.simulator.core.domain.regler.trygdetid.Brok
 import no.nav.pensjon.simulator.core.domain.reglerextend.copy
-import java.util.Date
+import no.nav.pensjon.simulator.core.domain.reglerextend.grunnlag.copy
+import java.util.*
+
+fun AldersberegningKapittel20.copy() =
+    AldersberegningKapittel20().also {
+        it.delingstall = this.delingstall
+        it.beholdninger = this.beholdninger?.copy()
+        it.pensjonUnderUtbetaling = this.pensjonUnderUtbetaling?.let(::PensjonUnderUtbetaling)
+        it.dtBenyttetArsakListe = this.dtBenyttetArsakListe.map(::FtDtArsak)
+        it.beholdningerForForsteuttak = this.beholdningerForForsteuttak
+        it.prorataBrok = this.prorataBrok?.let(::Brok)
+        copyBeregning2011(source = this, target = it)
+    }
 
 fun AvkortingsinformasjonBT.copy() =
     AvkortingsinformasjonBT().also {
@@ -111,9 +111,83 @@ fun BeregningsResultatAlderspensjon2011.copy() =
         copyBeregningsResultat(source = this, target = it)
     }
 
+fun FremskrivingsDetaljer.copy() =
+    FremskrivingsDetaljer().also {
+        it.justeringTomDato = this.justeringTomDato?.clone() as? Date
+        it.justeringsfaktor = this.justeringsfaktor
+        it.teller = this.teller
+        it.nevner = this.nevner
+        it.arskull = this.arskull
+    }
+
+fun GarantitilleggInformasjon.copy() =
+    GarantitilleggInformasjon().also {
+        it.anvendtSivilstandEnum = this.anvendtSivilstandEnum
+        it.pensjonsbeholdningBelopVed67 = this.pensjonsbeholdningBelopVed67
+        it.garantipensjonsbeholdningBelopVed67 = this.garantipensjonsbeholdningBelopVed67
+        it.tt_kapittel20Ved67 = this.tt_kapittel20Ved67
+        it.tt_kapittel19Ved67 = this.tt_kapittel19Ved67
+        it.pa_f92Ved67 = this.pa_f92Ved67
+        it.pa_e91Ved67 = this.pa_e91Ved67
+        it.ftVed67 = this.ftVed67
+        it.dtVed67 = this.dtVed67
+        it.sptVed67 = this.sptVed67
+        it.tt_2009 = this.tt_2009
+        it.pa_f92_2009 = this.pa_f92_2009
+        it.pa_e91_2009 = this.pa_e91_2009
+        it.spt_2009 = this.spt_2009
+        it.ft67_1962 = this.ft67_1962
+        it.dt67_1962 = this.dt67_1962
+        it.estimertTTkap19Benyttet = this.estimertTTkap19Benyttet
+    }
+
+fun JusteringsInformasjon.copy() =
+    JusteringsInformasjon().also {
+        it.totalJusteringsfaktor = this.totalJusteringsfaktor
+        it.justeringsTypeEnum = this.justeringsTypeEnum
+        it.elementer = this.elementer.map(::copyJustering).toMutableList()
+    }
+
+fun JustertGarantipensjonsniva.copy() =
+    JustertGarantipensjonsniva().also {
+        it.garantipensjonsniva = this.garantipensjonsniva
+        it.justeringsInformasjon = this.justeringsInformasjon
+        it.belop = this.belop
+    }
+
+fun LonnsvekstDetaljer.copy() =
+    LonnsvekstDetaljer().also {
+        it.justeringTomDato = this.justeringTomDato?.clone() as? Date
+        it.justeringsfaktor = this.justeringsfaktor
+        it.lonnsvekst = this.lonnsvekst
+    }
+
 fun TapendeBeregningsmetode.copy() =
     TapendeBeregningsmetode().also {
         it.beregningMetodeTypeEnum = this.beregningMetodeTypeEnum
+    }
+
+fun Uforetrygdberegning.copy() =
+    Uforetrygdberegning().also {
+        it.bruttoPerAr = this.bruttoPerAr
+        it.formelKodeEnum = this.formelKodeEnum
+        it.grunnbelop = this.grunnbelop
+        it.minsteytelse = this.minsteytelse?.let(::Minsteytelse)
+        it.prorataBrok = this.prorataBrok?.let(::Brok)
+        it.uforegrad = this.uforegrad
+        it.uforetidspunkt = this.uforetidspunkt?.clone() as? Date
+        it.egenopptjentUforetrygd = this.egenopptjentUforetrygd?.let(::EgenopptjentUforetrygd)
+        it.egenopptjentUforetrygdBest = this.egenopptjentUforetrygdBest
+        it.yrkesskadegrad = this.yrkesskadegrad
+        it.yrkesskadetidspunkt = this.yrkesskadetidspunkt?.clone() as? Date
+        it.mottarMinsteytelse = this.mottarMinsteytelse
+        //it.avtaleBeregningsmetode = this.avtaleBeregningsmetode
+        it.minsteytelseArsak = this.minsteytelseArsak
+        it.instOppholdTypeEnum = this.instOppholdTypeEnum
+        it.instOpphAnvendt = this.instOpphAnvendt
+        it.uforeEkstra = this.uforeEkstra?.let(::UforeEkstraUT)
+        it.ytelseVedDodEnum = this.ytelseVedDodEnum
+        copyBeregning2011(source = this, target = it)
     }
 
 private fun copyAvkortingsinformasjon(source: AbstraktAvkortingsinformasjon, target: AbstraktAvkortingsinformasjon) {
@@ -123,6 +197,25 @@ private fun copyAvkortingsinformasjon(source: AbstraktAvkortingsinformasjon, tar
     target.avkortingsbelopPerAr = source.avkortingsbelopPerAr
     target.restTilUtbetaling = source.restTilUtbetaling
     target.inntektsavkortingTypeEnum = source.inntektsavkortingTypeEnum
+}
+
+private fun copyBeregning2011(
+    source: Beregning2011,
+    target: Beregning2011,
+    copyDelberegninger: Boolean = true
+) {
+    target.gjelderPerson = source.gjelderPerson?.penPersonId?.let(::PenPerson)
+    target.grunnbelop = source.grunnbelop
+    target.tt_anv = source.tt_anv
+    target.resultatTypeEnum = source.resultatTypeEnum
+    target.beregningsMetodeEnum = source.beregningsMetodeEnum
+    target.beregningTypeEnum = source.beregningTypeEnum
+    target.merknadListe = source.merknadListe.map(::Merknad)
+    target.beregningGjelderTypeEnum = source.beregningGjelderTypeEnum
+
+    if (copyDelberegninger) {
+        target.delberegning2011Liste = source.delberegning2011Liste.map(::BeregningRelasjon)
+    }
 }
 
 private fun copyBeregningsResultat(source: AbstraktBeregningsResultat, target: AbstraktBeregningsResultat) {
@@ -141,3 +234,11 @@ private fun copyBeregningsResultat(source: AbstraktBeregningsResultat, target: A
     target.epsPaavirkerBeregning = source.epsPaavirkerBeregning
     target.harGjenlevenderett = source.harGjenlevenderett
 }
+
+private fun copyJustering(source: IJustering): IJustering =
+    when (source) {
+        is LonnsvekstDetaljer -> source.copy()
+        is FremskrivingsDetaljer -> source.copy()
+        is GReguleringDetaljer -> throw RuntimeException("Deprecated IJustering type: GReguleringDetaljer")
+        else -> throw RuntimeException("Unsupported IJustering type: ${source.javaClass.name}")
+    }

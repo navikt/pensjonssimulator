e2e32710d4090bfe253b32bc8f26bd8693b96038
Espen Gjøstøl <espen.oskar.gjostol@nav.no>
2025-03-12 15:12:08 +0100

    Add tests for SimuleringSpecValidator

--- src/test/kotlin/no/nav/pensjon/simulator/testutil/TestObjects.kt
+++ src/test/kotlin/no/nav/pensjon/simulator/testutil/TestObjects.kt
@@ -26,12 +26,13 @@ object TestObjects {
         type: SimuleringType = SimuleringType.ALDER_M_AFP_PRIVAT,
         sivilstatus: SivilstatusType = SivilstatusType.UGIF,
         epsHarPensjon: Boolean = false,
+        foersteUttakDato: LocalDate? = LocalDate.of(2029, 1, 1),
         inntektSpecListe: List<FremtidigInntekt> = emptyList()
     ) = SimuleringSpec(
         type,
         sivilstatus,
         epsHarPensjon,
-        foersteUttakDato = LocalDate.of(2029, 1, 1),
+        foersteUttakDato,
         heltUttakDato = LocalDate.of(2032, 6, 1),
         pid = pid,
         foedselDato = null,

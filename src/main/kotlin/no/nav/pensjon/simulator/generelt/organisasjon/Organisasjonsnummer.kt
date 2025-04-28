3778f09abcb343c933e911ad161c32640d353b85
Espen Gjøstøl <espen.oskar.gjostol@nav.no>
2025-04-28 12:15:46 +0200

    Use inline value class for PID and organisasjonsnummer

--- src/main/kotlin/no/nav/pensjon/simulator/generelt/organisasjon/Organisasjonsnummer.kt
+++ src/main/kotlin/no/nav/pensjon/simulator/generelt/organisasjon/Organisasjonsnummer.kt
@@ -1,7 +1,20 @@
 package no.nav.pensjon.simulator.generelt.organisasjon
 
-data class Organisasjonsnummer(
-    val value: String
-) {
+/**
+ * Representerer organisasjonsnummer (no.wikipedia.org/wiki/Organisasjonsnummer)
+ */
+@JvmInline
+value class Organisasjonsnummer(val value: String) {
+
+    init {
+        require(value.length == REQUIRED_LENGTH) {
+            "Feil lengde (${value.length}) på organisasjonsnummer; må være $REQUIRED_LENGTH"
+        }
+    }
+
     override fun toString(): String = value
+
+    companion object {
+        private const val REQUIRED_LENGTH = 9
+    }
 }
--- src/test/kotlin/no/nav/pensjon/simulator/generelt/organisasjon/OrganisasjonsnummerTest.kt
+++ src/test/kotlin/no/nav/pensjon/simulator/generelt/organisasjon/OrganisasjonsnummerTest.kt
@@ -1,11 +1,22 @@
 package no.nav.pensjon.simulator.generelt.organisasjon
 
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 
 class OrganisasjonsnummerTest : FunSpec({
 
-    test("toString returns the original organisasjonsnummer") {
+    test("toString should return the original organisasjonsnummer") {
         "Org: ${Organisasjonsnummer("123456789")}" shouldBe "Org: 123456789"
+    }
+
+    test("organisasjonsnummer should require length 9") {
+        shouldThrow<IllegalArgumentException> {
+            Organisasjonsnummer("")
+        }.message shouldBe "Feil lengde (0) på organisasjonsnummer; må være 9"
+
+        shouldThrow<IllegalArgumentException> {
+            Organisasjonsnummer("1234567890")
+        }.message shouldBe "Feil lengde (10) på organisasjonsnummer; må være 9"
     }
 })

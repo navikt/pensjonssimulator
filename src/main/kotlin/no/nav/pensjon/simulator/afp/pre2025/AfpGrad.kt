cca8b5d81f254fb0074a3ee770885b080ec960db
9maPiltan <nima.piltan@nav.no>
2025-04-28 11:13:17 +0200

    Endring basert på PR

--- src/main/kotlin/no/nav/pensjon/simulator/afp/pre2025/AfpGrad.kt
+++ src/main/kotlin/no/nav/pensjon/simulator/afp/pre2025/AfpGrad.kt
@@ -1,11 +1,9 @@
 package no.nav.pensjon.simulator.afp.pre2025
 
 object AfpGrad {
-    fun beregnAfpGrad(inntektVedAfpUttak: Int, tidligereInntekt: Int): Int {
-        return if (tidligereInntekt == 0) {
+    fun beregnAfpGrad(inntektVedAfpUttak: Int, tidligereInntekt: Int): Int =
+        if (tidligereInntekt == 0)
             0
-        } else {
+        else
             100 - ((inntektVedAfpUttak.toDouble() / tidligereInntekt) * 100).toInt()
-        }
-    }
 }

a5a638f8744d617526229abef9d34c8cfa3e5359
martinstorvoll <martin.storvoll@nav.no>
2025-05-09 10:36:05 +0200

    yml for apitoken

--- src/main/kotlin/no/nav/pensjon/simulator/tech/toggle/FeatureToggleService.kt
+++ src/main/kotlin/no/nav/pensjon/simulator/tech/toggle/FeatureToggleService.kt
@@ -1,10 +1,9 @@
 package no.nav.pensjon.simulator.tech.toggle
 
 import no.nav.pensjon.simulator.tech.toggle.client.FeatureToggleClient
-import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Service
 
 @Service
-class FeatureToggleService(@Qualifier("featureToggleClient") val client: FeatureToggleClient) {
+class FeatureToggleService(val client: FeatureToggleClient) {
     fun isEnabled(featureName: String) = client.isEnabled(featureName)
 }
--- src/main/kotlin/no/nav/pensjon/simulator/tech/toggle/client/unleash/UnleashFeatureToggleClient.kt
+++ src/main/kotlin/no/nav/pensjon/simulator/tech/toggle/client/unleash/UnleashFeatureToggleClient.kt
@@ -2,9 +2,11 @@ package no.nav.pensjon.simulator.tech.toggle.client.unleash
 
 import io.getunleash.Unleash
 import no.nav.pensjon.simulator.tech.toggle.client.FeatureToggleClient
+import org.springframework.context.annotation.Primary
 import org.springframework.stereotype.Component
 
 @Component
+@Primary
 class UnleashFeatureToggleClient(private val unleash: Unleash) : FeatureToggleClient {
     override fun isEnabled(featureName: String) = unleash.isEnabled(featureName)
 }

3d43e69ef26e45cc3d9633cc03801e7b3b72fde1
Anton Fofanov <anton.fofanov@nav.no>
2025-05-14 15:55:47 +0200

    fix import krøll

--- end-to-end-test/src/main/kotlin/client/ClientProvider.kt
+++ end-to-end-test/src/main/kotlin/client/ClientProvider.kt
@@ -1,29 +1,24 @@
 package no.nav.pensjon.client
 
-import io.ktor.client.HttpClient
-import io.ktor.client.HttpClientConfig
-import io.ktor.client.engine.cio.CIO
-import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
-import io.ktor.client.plugins.logging.DEFAULT
-import io.ktor.client.plugins.logging.LogLevel
-import io.ktor.client.plugins.logging.Logger
-import io.ktor.client.plugins.logging.Logging
-import io.ktor.client.plugins.logging.LoggingConfig
-import io.ktor.serialization.kotlinx.json.json
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.plugins.logging.*
+import io.ktor.serialization.kotlinx.json.*
 import kotlinx.serialization.json.Json
 
 object ClientProvider {
     val client = HttpClient(CIO) {
-        HttpClientConfig.install(ContentNegotiation) {
+        install(ContentNegotiation) {
             json(
                 Json {
                     ignoreUnknownKeys = true
                 }
             )
         }
-        HttpClientConfig.install(Logging) {
-            LoggingConfig.logger = Logger.Companion.DEFAULT
-            LoggingConfig.level = LogLevel.ALL
+        install(Logging) {
+            logger = Logger.Companion.DEFAULT
+            level = LogLevel.ALL
         }
     }
 

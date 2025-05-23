03dce9af49742130ae4fed3ddbdf0d25d8d1ae32
Espen Gjøstøl <espen.oskar.gjostol@nav.no>
2025-05-23 14:13:17 +0200

    Delete unused classes

--- src/main/kotlin/no/nav/pensjon/simulator/core/domain/regler/gomregning/KravFaktoromregningGrunnlag.kt
+++
@@ -1,6 +0,0 @@
-package no.nav.pensjon.simulator.core.domain.regler.gomregning
-
-class KravFaktoromregningGrunnlag(
-    var kravId: Long? = null,
-    var persongrunnlagOmregningGrunnlagListe: MutableList<PersongrunnlagOmregningGrunnlag> = mutableListOf()
-)
--- src/main/kotlin/no/nav/pensjon/simulator/core/domain/regler/gomregning/PersongrunnlagOmregningGrunnlag.kt
+++
@@ -1,9 +0,0 @@
-package no.nav.pensjon.simulator.core.domain.regler.gomregning
-
-import no.nav.pensjon.simulator.core.domain.regler.grunnlag.Inntektsgrunnlag
-
-class PersongrunnlagOmregningGrunnlag(
-    var persongrunnlagId: Long? = null,
-    var inntektsgrunnlagGrunnlagListe: MutableList<Inntektsgrunnlag> = mutableListOf(),
-    var lonnsvekstOmregn: Boolean = false
-)

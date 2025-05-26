689d6883b3ce5af76061845f3be168bbf7f27ee5
Espen Gjøstøl <espen.oskar.gjostol@nav.no>
2025-05-26 11:56:07 +0200

    Minor import change in InntektService

--- src/main/kotlin/no/nav/pensjon/simulator/inntekt/InntektService.kt
+++ src/main/kotlin/no/nav/pensjon/simulator/inntekt/InntektService.kt
@@ -3,7 +3,7 @@ package no.nav.pensjon.simulator.inntekt
 import no.nav.pensjon.simulator.opptjening.SisteLignetInntekt
 import no.nav.pensjon.simulator.person.Pid
 import no.nav.pensjon.simulator.regel.client.RegelClient
-import no.nav.pensjon.simulator.tech.time.DateUtil
+import no.nav.pensjon.simulator.tech.time.DateUtil.MAANEDER_PER_AAR
 import no.nav.pensjon.simulator.tech.time.Time
 import org.springframework.stereotype.Service
 
@@ -18,7 +18,7 @@ class InntektService(
 
     fun hentSisteMaanedsInntektOver1G(harInntektSisteMaanedOver1G: Boolean): Int =
         if (harInntektSisteMaanedOver1G)
-            naavaerendeGrunnbeloep() * TILFELDIG_ANTALL_G_STOERRE_ENN_EN / DateUtil.MAANEDER_PER_AAR
+            naavaerendeGrunnbeloep() * TILFELDIG_ANTALL_G_STOERRE_ENN_EN / MAANEDER_PER_AAR
         else
             TILFELDIG_MAANEDSINNTEKT_STOERRE_ENN_NULL
 

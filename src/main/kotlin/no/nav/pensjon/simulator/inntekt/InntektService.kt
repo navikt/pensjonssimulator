f48c87e343c716a2bdc614c045954bf96b9d2016
Espen Gjøstøl <espen.oskar.gjostol@nav.no>
2025-05-27 12:47:24 +0200

    Use GrunnbeloepService in InntektService

--- src/main/kotlin/no/nav/pensjon/simulator/inntekt/InntektService.kt
+++ src/main/kotlin/no/nav/pensjon/simulator/inntekt/InntektService.kt
@@ -1,29 +1,24 @@
 package no.nav.pensjon.simulator.inntekt
 
+import no.nav.pensjon.simulator.g.GrunnbeloepService
 import no.nav.pensjon.simulator.opptjening.SisteLignetInntekt
 import no.nav.pensjon.simulator.person.Pid
-import no.nav.pensjon.simulator.regel.client.RegelClient
 import no.nav.pensjon.simulator.tech.time.DateUtil.MAANEDER_PER_AAR
-import no.nav.pensjon.simulator.tech.time.Time
 import org.springframework.stereotype.Service
 
 @Service
 class InntektService(
     private val lignetInntektService: SisteLignetInntekt,
-    private val regelClient: RegelClient,
-    private val time: Time
+    private val grunnbeloepService: GrunnbeloepService
 ) {
     fun hentSisteLignetInntekt(pid: Pid): Int =
         lignetInntektService.hentSisteLignetInntekt(pid).aarligBeloep
 
     fun hentSisteMaanedsInntektOver1G(harInntektSisteMaanedOver1G: Boolean): Int =
         if (harInntektSisteMaanedOver1G)
-            naavaerendeGrunnbeloep() * TILFELDIG_ANTALL_G_STOERRE_ENN_EN / MAANEDER_PER_AAR
+            grunnbeloepService.naavaerendeGrunnbeloep() * TILFELDIG_ANTALL_G_STOERRE_ENN_EN / MAANEDER_PER_AAR
         else
             TILFELDIG_MAANEDSINNTEKT_STOERRE_ENN_NULL
-
-    private fun naavaerendeGrunnbeloep(): Int =
-        regelClient.fetchGrunnbeloepListe(time.today()).satsResultater.first().verdi.toInt()
 
     private companion object {
         private const val TILFELDIG_MAANEDSINNTEKT_STOERRE_ENN_NULL = 42

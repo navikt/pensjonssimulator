0e656009dcdced532953c4abdd97a9ccadda3e16
Espen Gjøstøl <espen.oskar.gjostol@nav.no>
2025-06-19 11:04:40 +0200

    Keep heltUttakDato null if ugradert uttak

--- src/test/kotlin/no/nav/pensjon/simulator/alderspensjon/api/nav/direct/acl/v3/spec/NavSimuleringSpecMapperV3Test.kt
+++ src/test/kotlin/no/nav/pensjon/simulator/alderspensjon/api/nav/direct/acl/v3/spec/NavSimuleringSpecMapperV3Test.kt
@@ -40,7 +40,7 @@ class NavSimuleringSpecMapperV3Test : FunSpec({
                     aarligInntekt = 5000,
                     inntektTomAlder = NavSimuleringAlderSpecV3(aar = 74, maaneder = 5)
                 ),
-                aarUtenlandsEtter16Aar = 8,
+                aarUtenlandsEtter16Aar = 8, // ignoreres; i denne kontekst brukes utenlandsperiodeListe
                 epsHarPensjon = true,
                 epsHarInntektOver2G = false,
                 fremtidigInntektListe = listOf(
@@ -74,8 +74,8 @@ class NavSimuleringSpecMapperV3Test : FunSpec({
             inntektUnderGradertUttakBeloep = 4000,
             inntektEtterHeltUttakBeloep = 5000,
             inntektEtterHeltUttakAntallAar = 9,
-            foedselAar = 0,
-            utlandAntallAar = 0,
+            foedselAar = 0, // brukes ikke i denne kontekst
+            utlandAntallAar = 0, // siden utlandPeriodeListe brukes isteden
             utlandPeriodeListe = mutableListOf(
                 UtlandPeriode(
                     fom = LocalDate.of(2021, 1, 1),
@@ -95,6 +95,64 @@ class NavSimuleringSpecMapperV3Test : FunSpec({
             ignoreAvslag = false,
             isHentPensjonsbeholdninger = false,
             isOutputSimulertBeregningsinformasjonForAllKnekkpunkter = true, // to produce månedsbeløp
+            onlyVilkaarsproeving = false,
+            epsKanOverskrives = true
+        )
+    }
+
+    test("fromNavSimuleringSpecV3 should use heltUttakDato = null if ugradert uttak") {
+        NavSimuleringSpecMapperV3(
+            personService = Arrange.foedselsdato(1963, 4, 5),
+            inntektService = arrangeGrunnbeloep()
+        ).fromNavSimuleringSpecV3(
+            source = NavSimuleringSpecV3(
+                pid.value,
+                sivilstand = NavSivilstandSpecV3.ENKE,
+                uttaksar = 0,
+                sisteInntekt = 2000,
+                simuleringstype = NavSimuleringTypeSpecV3.ENDR_AP_M_AFP_OFFENTLIG_LIVSVARIG,
+                gradertUttak = null, // => ugradert uttak
+                heltUttak = NavSimuleringHeltUttakSpecV3(
+                    uttakFomAlder = NavSimuleringAlderSpecV3(aar = 66, maaneder = 7),
+                    aarligInntekt = 5000,
+                    inntektTomAlder = NavSimuleringAlderSpecV3(aar = 73, maaneder = 0)
+                ),
+                aarUtenlandsEtter16Aar = 0,
+                epsHarPensjon = false,
+                epsHarInntektOver2G = true,
+                fremtidigInntektListe = emptyList(),
+                utenlandsperiodeListe = emptyList()
+            ),
+        ) shouldBe SimuleringSpec(
+            type = SimuleringTypeEnum.ENDR_AP_M_AFP_OFFENTLIG_LIVSVARIG,
+            sivilstatus = SivilstatusType.ENKE,
+            epsHarPensjon = false,
+            foersteUttakDato = LocalDate.of(2029, 12, 1), // dato for helt uttak
+            heltUttakDato = null, // siden ugradert uttak
+            pid = pid,
+            foedselDato = LocalDate.of(1963, 4, 5),
+            avdoed = null,
+            isTpOrigSimulering = false,
+            simulerForTp = false,
+            uttakGrad = UttakGradKode.P_100,
+            forventetInntektBeloep = 2000,
+            inntektUnderGradertUttakBeloep = 0,
+            inntektEtterHeltUttakBeloep = 5000,
+            inntektEtterHeltUttakAntallAar = 8,
+            foedselAar = 0,
+            utlandAntallAar = 0,
+            utlandPeriodeListe = mutableListOf(),
+            fremtidigInntektListe = mutableListOf(),
+            brukFremtidigInntekt = false,
+            inntektOver1GAntallAar = 0,
+            flyktning = false,
+            epsHarInntektOver2G = true,
+            rettTilOffentligAfpFom = null,
+            pre2025OffentligAfp = null,
+            erAnonym = false,
+            ignoreAvslag = false,
+            isHentPensjonsbeholdninger = false,
+            isOutputSimulertBeregningsinformasjonForAllKnekkpunkter = true,
             onlyVilkaarsproeving = false,
             epsKanOverskrives = true
         )

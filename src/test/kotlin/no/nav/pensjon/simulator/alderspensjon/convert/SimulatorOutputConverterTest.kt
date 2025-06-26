99446b7d21a2f239cc14b28a4cc2fbcdce307460
Espen Gjøstøl <espen.oskar.gjostol@nav.no>
2025-06-26 12:14:36 +0200

    Use trygdetid = max of kapittel 19 and kapittel 20

--- src/test/kotlin/no/nav/pensjon/simulator/alderspensjon/convert/SimulatorOutputConverterTest.kt
+++ src/test/kotlin/no/nav/pensjon/simulator/alderspensjon/convert/SimulatorOutputConverterTest.kt
@@ -23,6 +23,7 @@ class SimulatorOutputConverterTest : FunSpec({
      * Also tests that:
      * - harUttak = false if no uttaksgrad covers today's date
      * - harNokTrygdetidForGarantipensjon = false if mindre enn 5 år 'kapittel 20'-trygdetid
+     * - trygdetid = max av kapittel 19-trygdetid og kapittel 20-trygdetid
      */
     test("'pensjon' should map SimulatorOutput to SimulertPensjon") {
         SimulatorOutputConverter.pensjon(
@@ -106,7 +107,7 @@ class SimulatorOutputConverterTest : FunSpec({
             pensjonBeholdningPeriodeListe = emptyList(),
             harUttak = false,
             harNokTrygdetidForGarantipensjon = false,
-            trygdetid = 4, // NB: Mapped twice (also to trygdetidKap20)
+            trygdetid = 19, // NB: Max of trygdetidKap19 and trygdetidKap20
             opptjeningGrunnlagListe = emptyList()
         )
     }

10068cdcc680ae29a482ff43108229124295ed8e
Espen Gjøstøl <espen.oskar.gjostol@nav.no>
2025-08-21 16:48:45 +0200

    Use 'simuleringstype' deducer

---
+++ src/main/kotlin/no/nav/pensjon/simulator/alderspensjon/spec/OffentligSimuleringstypeDeducer.kt
@@ -0,0 +1,52 @@
+package no.nav.pensjon.simulator.alderspensjon.spec
+
+import no.nav.pensjon.simulator.core.domain.regler.enum.SimuleringTypeEnum
+import no.nav.pensjon.simulator.core.exception.FeilISimuleringsgrunnlagetException
+import no.nav.pensjon.simulator.person.Pid
+import no.nav.pensjon.simulator.vedtak.VedtakService
+import no.nav.pensjon.simulator.vedtak.VedtakStatus
+import org.springframework.stereotype.Component
+import java.time.LocalDate
+
+/**
+ * Utleder simuleringstype i "offentlig" sammenheng, dvs. at f.eks. privat AFP ikke tas hensyn til.
+ * Det tas heller ikke hensyn til "gammel" offentlig AFP, kun "ny" (livsvarig) AFP.
+ */
+@Component
+class OffentligSimuleringstypeDeducer(private val vedtakService: VedtakService) {
+
+    fun deduceSimuleringstype(
+        pid: Pid,
+        uttakFom: LocalDate,
+        livsvarigOffentligAfpRettFom: LocalDate? = null
+    ): SimuleringTypeEnum {
+        val vedtakInfo = vedtakService.vedtakStatus(pid, uttakFom).also(::checkForGjenlevenderettighet)
+
+        return deduceSimuleringstype(
+            erFoerstegangsuttak = vedtakInfo.harGjeldendeVedtak.not(),
+            livsvarigOffentligAfpRettDato = livsvarigOffentligAfpRettFom
+        )
+    }
+
+    private companion object {
+        // PEN: SimuleringServiceBase.checkForGjenlevenderettighet
+        private fun checkForGjenlevenderettighet(vedtakInfo: VedtakStatus) {
+            if (vedtakInfo.harGjenlevenderettighet)
+                throw FeilISimuleringsgrunnlagetException("Kan ikke simulere bruker med gjenlevenderettigheter")
+        }
+
+        private fun deduceSimuleringstype(
+            erFoerstegangsuttak: Boolean,
+            livsvarigOffentligAfpRettDato: LocalDate?
+        ): SimuleringTypeEnum =
+            livsvarigOffentligAfpRettDato?.let {
+                if (erFoerstegangsuttak)
+                    SimuleringTypeEnum.ALDER_MED_AFP_OFFENTLIG_LIVSVARIG
+                else
+                    SimuleringTypeEnum.ENDR_AP_M_AFP_OFFENTLIG_LIVSVARIG
+            } ?: if (erFoerstegangsuttak)
+                SimuleringTypeEnum.ALDER
+            else
+                SimuleringTypeEnum.ENDR_ALDER
+    }
+}

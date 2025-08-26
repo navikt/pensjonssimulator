42c89ba6b8c1e938ef2e85f6dfe58f15bd97b250
Espen Gjøstøl <espen.oskar.gjostol@nav.no>
2025-08-26 11:29:30 +0200

    Update enums according to pensjon-regler-api v1.1.25

--- src/main/kotlin/no/nav/pensjon/simulator/core/domain/regler/enum/GPSatsTypeEnum.kt
+++ src/main/kotlin/no/nav/pensjon/simulator/core/domain/regler/enum/GPSatsTypeEnum.kt
@@ -1,5 +1,9 @@
 package no.nav.pensjon.simulator.core.domain.regler.enum
 
+/**
+ * pensjon-regler-api: no/nav/pensjon/regler/domain/enum/GPSatsTypeEnum.kt
+ * Copied 2025-08-26
+ */
 enum class GPSatsTypeEnum {
     FORHOYET,
     ORDINAER

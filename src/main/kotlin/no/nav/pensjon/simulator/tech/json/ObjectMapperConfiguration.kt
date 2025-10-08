a2a76b51de2fab28e6e0cfbe05c4c4d95cd9e03e
Anton Fofanov <anton.fofanov@nav.no>
2025-10-08 17:27:12 +0200

    TPP-8 plugg inn jackson module for kotlin

--- src/main/kotlin/no/nav/pensjon/simulator/tech/json/ObjectMapperConfiguration.kt
+++ src/main/kotlin/no/nav/pensjon/simulator/tech/json/ObjectMapperConfiguration.kt
@@ -8,6 +8,7 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Primary
@@ -65,6 +66,7 @@ open class ObjectMapperConfiguration {
 
                 registerModule(JavaTimeModule())
             }
+            .registerKotlinModule()
 
     /**
      * Mapper that fails on unknown properties.

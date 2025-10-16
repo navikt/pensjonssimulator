cc38fcd891e7d79ff4bf6d5dbe8209f5b016b3f8
Espen Gjøstøl <espen.oskar.gjostol@nav.no>
2025-10-16 13:01:55 +0200

    TPP-43 Log invalid scope

--- src/main/kotlin/no/nav/pensjon/simulator/tech/security/ingress/scope/ScopeMapper.kt
+++ src/main/kotlin/no/nav/pensjon/simulator/tech/security/ingress/scope/ScopeMapper.kt
@@ -1,9 +1,12 @@
 package no.nav.pensjon.simulator.tech.security.ingress.scope
 
+import mu.KotlinLogging
 import org.springframework.security.core.GrantedAuthority
 import org.springframework.security.core.authority.SimpleGrantedAuthority
 
 object ScopeMapper {
+
+    private val log = KotlinLogging.logger {}
 
     fun tokenScopeToAuthorities(tokenScope: String): Collection<GrantedAuthority> =
         when (tokenScope) {
@@ -11,7 +14,7 @@ object ScopeMapper {
             "nav:pensjon/simulering.read" -> authorities(internalScope = "simuler-alderspensjon")
             "nav:pensjon/v3/alderspensjon" -> authorities(internalScope = "simuler-alderspensjon")
             "nav:pensjonssimulator:simulering" -> authorities(internalScope = "simuler-alderspensjon")
-            else -> emptyList()
+            else -> emptyList<GrantedAuthority>().also { log.info { "Ugyldig scope '$tokenScope'" } }
         }
 
     private fun authorities(internalScope: String): List<SimpleGrantedAuthority> =

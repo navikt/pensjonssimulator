3324cbf8d24f527fe3279445f4d43c5735d6cbd2
Espen Gjøstøl <espen.oskar.gjostol@nav.no>
2025-10-29 16:23:11 +0100

    TPP-54 Support gjenlevendrett for 'endring alderspensjon'

--- src/main/kotlin/no/nav/pensjon/simulator/core/ytelse/LoependeYtelser.kt
+++ src/main/kotlin/no/nav/pensjon/simulator/core/ytelse/LoependeYtelser.kt
@@ -3,14 +3,15 @@ package no.nav.pensjon.simulator.core.ytelse
 import no.nav.pensjon.simulator.core.domain.regler.beregning2011.AbstraktBeregningsResultat
 import no.nav.pensjon.simulator.core.domain.regler.beregning2011.SisteBeregning
 import no.nav.pensjon.simulator.core.domain.regler.vedtak.VilkarsVedtak
+import no.nav.pensjon.simulator.ytelse.AvdoedYtelser
 import java.time.LocalDate
 
 data class LoependeYtelser(
     val soekerVirkningFom: LocalDate,
-    val avdoedVirkningFom: LocalDate?,
     val privatAfpVirkningFom: LocalDate?,
     val sisteBeregning: SisteBeregning?,
     val forrigeAlderspensjonBeregningResultat: AbstraktBeregningsResultat?,
     val forrigePrivatAfpBeregningResultat: AbstraktBeregningsResultat?,
-    val forrigeVedtakListe: MutableList<VilkarsVedtak>
+    val forrigeVedtakListe: MutableList<VilkarsVedtak>,
+    val avdoed: AvdoedYtelser?
 )

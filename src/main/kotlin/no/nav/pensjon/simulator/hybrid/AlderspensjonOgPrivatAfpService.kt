38ac0910c3c452507a416fd62895bdefd06ddf99
Espen Gjøstøl <50329374+espengjostol@users.noreply.github.com>
2026-01-13 10:41:35 +0100

    TPP-109 Use error code in hybrid response (#308)
    Also:
    TPP-64 Add 'harLoependePrivatAfp' field

---
+++ src/main/kotlin/no/nav/pensjon/simulator/hybrid/AlderspensjonOgPrivatAfpService.kt
@@ -0,0 +1,77 @@
+package no.nav.pensjon.simulator.hybrid
+
+import no.nav.pensjon.simulator.core.SimulatorCore
+import no.nav.pensjon.simulator.core.exception.*
+import no.nav.pensjon.simulator.core.spec.SimuleringSpec
+import no.nav.pensjon.simulator.tech.validation.InvalidEnumValueException
+import no.nav.pensjon.simulator.tech.web.BadRequestException
+import no.nav.pensjon.simulator.tech.web.EgressException
+import no.nav.pensjon.simulator.ytelse.YtelseService
+import org.springframework.stereotype.Service
+import java.time.format.DateTimeParseException
+
+/**
+ * Simuler alderspensjon og privat AFP.
+ * Dette er en "hybrid" tjeneste, siden to pensjonstyper er involvert.
+ */
+@Service
+class AlderspensjonOgPrivatAfpService(
+    private val simulatorCore: SimulatorCore,
+    private val ytelseService: YtelseService,
+    private val resultPreparer: AlderspensjonOgPrivatAfpResultPreparer
+) {
+    fun simuler(spec: SimuleringSpec): AlderspensjonOgPrivatAfpResult =
+        try {
+            resultPreparer.result(
+                simulatorOutput = simulatorCore.simuler(initialSpec = spec),
+                pid = spec.pid!!,
+                harLoependePrivatAfp = ytelseService.getLoependeYtelser(spec).privatAfpVirkningFom != null
+            )
+            //TODO: PEN: UgyldigInput - Jira TPP-47
+            //TODO PEN222BeregningstjenesteFeiletException, PEN223BrukerHarIkkeLopendeAlderspensjonException, PEN226BrukerHarLopendeAPPaGammeltRegelverkException - Jira TPP-44
+            //TODO Kopier ThrowableExceptionMapper fra PEN - Jira TPP-45
+        } catch (e: BadRequestException) {
+            problem(e, type = ProblemType.ANNEN_KLIENTFEIL)
+        } catch (e: BadSpecException) {
+            problem(e, type = ProblemType.ANNEN_KLIENTFEIL)
+        } catch (e: DateTimeParseException) {
+            problem(e, type = ProblemType.ANNEN_KLIENTFEIL)
+        } catch (e: EgressException) {
+            problem(e, type = ProblemType.SERVERFEIL)
+        } catch (e: FeilISimuleringsgrunnlagetException) {
+            problem(e, type = ProblemType.ANNEN_KLIENTFEIL)
+        } catch (e: ImplementationUnrecoverableException) {
+            problem(e, type = ProblemType.SERVERFEIL)
+        } catch (e: InvalidArgumentException) {
+            problem(e, type = ProblemType.ANNEN_KLIENTFEIL)
+        } catch (e: InvalidEnumValueException) {
+            problem(e, type = ProblemType.ANNEN_KLIENTFEIL)
+        } catch (e: KanIkkeBeregnesException) {
+            problem(e, type = ProblemType.ANNEN_KLIENTFEIL)
+        } catch (e: KonsistensenIGrunnlagetErFeilException) {
+            problem(e, type = ProblemType.ANNEN_KLIENTFEIL)
+        } catch (e: PersonForGammelException) {
+            problem(e, type = ProblemType.PERSON_FOR_HOEY_ALDER)
+        } catch (e: PersonForUngException) {
+            problem(e, type = ProblemType.ANNEN_KLIENTFEIL)
+        } catch (e: RegelmotorValideringException) {
+            problem(e, type = ProblemType.ANNEN_KLIENTFEIL)
+        } catch (e: UtilstrekkeligOpptjeningException) {
+            problem(e, type = ProblemType.UTILSTREKKELIG_OPPTJENING)
+        } catch (e: UtilstrekkeligTrygdetidException) {
+            problem(e, type = ProblemType.UTILSTREKKELIG_TRYGDETID)
+        }
+
+    private companion object {
+        private fun problem(e: RuntimeException, type: ProblemType) =
+            AlderspensjonOgPrivatAfpResult(
+                suksess = false,
+                alderspensjonsperiodeListe = emptyList(),
+                privatAfpPeriodeListe = emptyList(),
+                harNaavaerendeUttak = false,
+                harTidligereUttak = false,
+                harLoependePrivatAfp = false,
+                problem = Problem(type, beskrivelse = e.message ?: "Ukjent feil - ${e.javaClass.simpleName}")
+            )
+    }
+}
--- src/main/kotlin/no/nav/pensjon/simulator/hybrid/api/samhandler/acl/v3/AlderspensjonOgPrivatAfpResultV3.kt
+++ src/main/kotlin/no/nav/pensjon/simulator/hybrid/api/samhandler/acl/v3/AlderspensjonOgPrivatAfpResultV3.kt
@@ -1,5 +1,7 @@
 package no.nav.pensjon.simulator.hybrid.api.samhandler.acl.v3
 
+import org.springframework.http.HttpStatus
+
 /**
  * Version 3 of the data transfer object representing a result of the 'simuler alderspensjon & privat AFP' service.
  * ----------
@@ -7,11 +9,34 @@ package no.nav.pensjon.simulator.hybrid.api.samhandler.acl.v3
  *   + AFP part of no.nav.tjeneste.ekstern.simulerepensjon.v1.meldinger.HentSimulertPensjonResponse
  */
 data class AlderspensjonOgPrivatAfpResultV3(
+    val suksess: Boolean,
     val alderspensjonsperioder: List<ApOgPrivatAfpAlderspensjonsperiodeResultV3>,
     val privatAfpPerioder: List<ApOgPrivatAfpPrivatAfpPeriodeResultV3>, // from HentSimulertPensjonResponse
     val harNaavaerendeUttak: Boolean,
-    val harTidligereUttak: Boolean
+    val harTidligereUttak: Boolean,
+    val harLoependePrivatAfp: Boolean,
+    val problem: ProblemV3? = null
 )
+
+data class ProblemV3(
+    val kode: ProblemTypeV3,
+    val beskrivelse: String
+)
+
+enum class ProblemTypeV3(val httpStatus: HttpStatus = HttpStatus.BAD_REQUEST) {
+    UGYLDIG_UTTAKSDATO,
+    UGYLDIG_UTTAKSGRAD,
+    UGYLDIG_SIVILSTATUS,
+    UGYLDIG_INNTEKT,
+    UGYLDIG_ANTALL_AAR,
+    UGYLDIG_PERSONIDENT,
+    PERSON_IKKE_FUNNET(httpStatus = HttpStatus.NOT_FOUND),
+    PERSON_FOR_HOEY_ALDER,
+    UTILSTREKKELIG_OPPTJENING(httpStatus = HttpStatus.OK),
+    UTILSTREKKELIG_TRYGDETID(httpStatus = HttpStatus.OK),
+    ANNEN_KLIENTFEIL,
+    SERVERFEIL(httpStatus = HttpStatus.INTERNAL_SERVER_ERROR)
+}
 
 /**
  * Version 3 of 'alderspensjonsperiode' in result of 'simuler alderspensjon & privat AFP'.

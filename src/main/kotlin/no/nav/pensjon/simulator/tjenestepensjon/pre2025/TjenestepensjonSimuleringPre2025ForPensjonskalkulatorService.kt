529ff3a012969fe8f7deb963851226f35cffa7e6
Espen Gjøstøl <50329374+espengjostol@users.noreply.github.com>
2026-02-25 16:10:35 +0100

    Reduce log level for 'simuler TP pre-2025' (#389)
    (from Info to Debug)
    
    Also:
    * Increase encapsulation (make things private)
    * Extract error handling to separate function
    * Use ShouldSpec for tests

--- src/main/kotlin/no/nav/pensjon/simulator/tjenestepensjon/pre2025/TjenestepensjonSimuleringPre2025ForPensjonskalkulatorService.kt
+++ src/main/kotlin/no/nav/pensjon/simulator/tjenestepensjon/pre2025/TjenestepensjonSimuleringPre2025ForPensjonskalkulatorService.kt
@@ -10,8 +10,8 @@ import no.nav.pensjon.simulator.tech.toggle.FeatureToggleService.Companion.PEK_1
 import no.nav.pensjon.simulator.tech.web.EgressException
 import no.nav.pensjon.simulator.tjenestepensjon.pre2025.SimulerOffentligTjenestepensjonResult.Companion.ikkeMedlem
 import no.nav.pensjon.simulator.tjenestepensjon.pre2025.SimulerOffentligTjenestepensjonResult.Companion.tpOrdningStoettesIkke
+import no.nav.pensjon.simulator.tjenestepensjon.pre2025.metrics.SPKResultatKodePre2025
 import no.nav.pensjon.simulator.tjenestepensjon.pre2025.metrics.SPKResultatKodePre2025.*
-import no.nav.pensjon.simulator.tjenestepensjon.pre2025.metrics.SPKResultatKodePre2025.Companion.fromFeilkode
 import no.nav.pensjon.simulator.tjenestepensjon.pre2025.simulering.SPKTjenestepensjonServicePre2025
 import no.nav.pensjon.simulator.tjenestepensjon.pre2025.simulering.TjenestepensjonSimuleringPre2025Spec
 import no.nav.pensjon.simulator.tjenestepensjon.pre2025.stillingsprosent.SpkStillingsprosentService
@@ -22,17 +22,20 @@ import org.apache.el.parser.ParseException
 import org.springframework.stereotype.Component
 import java.time.LocalDate
 
+/**
+ * Service for V3 in TjenestepensjonPre2025Controller.
+ */
 @Component
 class TjenestepensjonSimuleringPre2025ForPensjonskalkulatorService(
-    val tpregisteretClient: TpregisteretClient,
-    val spkStillingsprosentService: SpkStillingsprosentService,
-    val spkTjenestepensjonServicePre2025: SPKTjenestepensjonServicePre2025,
+    private val tpregisteretClient: TpregisteretClient,
+    private val spkStillingsprosentService: SpkStillingsprosentService,
+    private val spkTjenestepensjonServicePre2025: SPKTjenestepensjonServicePre2025,
     private val featureToggleService: FeatureToggleService
 ) {
     val log = KotlinLogging.logger { }
 
     fun simuler(spec: TjenestepensjonSimuleringPre2025Spec): SimulerOffentligTjenestepensjonResult {
-        log.info { "Simulering av tjenestepensjon pre 2025: ${filterFnr(spec.toString())}" }
+        log.debug { "Simulering av tjenestepensjon pre 2025: ${filterFnr(spec.toString())}" }
         var spkMedlemskap: TpOrdning? = null
 
         try {
@@ -90,42 +93,53 @@ class TjenestepensjonSimuleringPre2025ForPensjonskalkulatorService(
             log.debug { "Returning response: ${filterFnr(response.toString())}" }
             return response
         } catch (e: EgressException) {
-            val errorCode: String?
-            val errorMessage: String?
-            try {
-                errorCode = JSONObjectUtils.parse(e.message)["errorCode"]?.toString()
-                errorMessage = JSONObjectUtils.parse(e.message)["message"]?.toString()
-            } catch (parseException: ParseException) {
-                log.error(parseException) { "Feil med parsing av koder fra SPK: ${parseException.message}" }
-                    .also { Metrics.countTjenestepensjonSimuleringPre2025(UKJENT_FEIL_HOS_SPK) }
-                throw RuntimeException("Feil med parsing av koder: ${parseException.message}")
-            }
-
             log.warn(e) { "Feilmelding i respons fra SPK: $e" }
 
-            if (spkMedlemskap == null || errorCode == null || errorMessage == null){
-                Metrics.countTjenestepensjonSimuleringPre2025(UKJENT_FEIL_HOS_SPK)
-                throw e
-            }
+            return spkMedlemskap?.let {
+                resultatVedFeil(
+                    feilkode = feilkode(exception = e),
+                    tpOrdning = it,
+                    foedselsdato = spec.foedselsdato
+                )
+            } ?: ukjentFeil(exception = e)
+        }
+    }
 
-            val feilkode = Feilkode.fromExternalValue(errorCode, errorMessage)
-                .also { Metrics.countTjenestepensjonSimuleringPre2025(fromFeilkode(it)) }
+    private fun feilkode(exception: EgressException): Feilkode =
+        try {
+            val externalErorCode = extractValue(exception, field = "errorCode") ?: ukjentFeil(exception)
+            val externalErrorMessage = extractValue(exception, field = "message") ?: ukjentFeil(exception)
 
+            Feilkode.fromExternalValue(externalErorCode, externalErrorMessage)
+                .also { count(SPKResultatKodePre2025.fromFeilkode(it)) }
+        } catch (e: ParseException) {
+            log.error(e) { "Feil med parsing av koder fra SPK: ${e.message}" }
+                .also { count(UKJENT_FEIL_HOS_SPK) }
+            throw RuntimeException("Feil med parsing av koder: ${e.message}")
+        }
+
+    private companion object {
+        private val FNR_REGEX = """[0-9]{11}""".toRegex()
+
+        private fun resultatVedFeil(
+            feilkode: Feilkode,
+            tpOrdning: TpOrdning,
+            foedselsdato: LocalDate
+        ): SimulerOffentligTjenestepensjonResult =
             if (feilkode == Feilkode.BEREGNING_GIR_NULL_UTBETALING)
-                return SimulerOffentligTjenestepensjonResult(
-                    tpnr = spkMedlemskap.tpNr,
-                    navnOrdning = spkMedlemskap.navn,
-                    feilkode = feilkode,
+                resultatVedFeil(
+                    feilkode,
+                    tpOrdning,
                     utbetalingsperiodeListe = listOf(
                         Utbetalingsperiode(
-                            datoFom = Alder.fromAlder(spec.foedselsdato, Alder(65, 0)),
-                            datoTom = Alder.fromAlder(spec.foedselsdato, Alder(67, 0)),
+                            datoFom = datoVedAlder(alderAar = 65, foedselsdato),
+                            datoTom = datoVedAlder(alderAar = 67, foedselsdato),
                             uttaksgrad = 100,
                             arligUtbetaling = 0.0,
                             ytelsekode = YtelseCode.AFP
                         ),
                         Utbetalingsperiode(
-                            datoFom = Alder.fromAlder(spec.foedselsdato, Alder(67, 0)),
+                            datoFom = datoVedAlder(alderAar = 67, foedselsdato),
                             datoTom = null,
                             uttaksgrad = 100,
                             arligUtbetaling = 0.0,
@@ -133,19 +147,41 @@ class TjenestepensjonSimuleringPre2025ForPensjonskalkulatorService(
                         )
                     )
                 )
+            else
+                resultatVedFeil(
+                    feilkode,
+                    tpOrdning,
+                    utbetalingsperiodeListe = emptyList()
+                )
 
-            return SimulerOffentligTjenestepensjonResult(
-                tpnr = spkMedlemskap.tpNr,
-                navnOrdning = spkMedlemskap.navn,
+        private fun resultatVedFeil(
+            feilkode: Feilkode,
+            tpOrdning: TpOrdning,
+            utbetalingsperiodeListe: List<Utbetalingsperiode>
+        ) =
+            SimulerOffentligTjenestepensjonResult(
+                tpnr = tpOrdning.tpNr,
+                navnOrdning = tpOrdning.navn,
                 feilkode = feilkode,
-                utbetalingsperiodeListe = emptyList()
+                utbetalingsperiodeListe = utbetalingsperiodeListe
             )
-        }
-    }
 
-    companion object {
-            val FNR_REGEX = """[0-9]{11}""".toRegex()
-            fun filterFnr(s: String) = FNR_REGEX.replace(s, "*****")
+        private fun datoVedAlder(alderAar: Int, foedselsdato: LocalDate): LocalDate =
+            Alder.fromAlder(foedselsdato, Alder(alderAar, maaneder = 0))
+
+        private fun extractValue(exception: EgressException, field: String): String? =
+            JSONObjectUtils.parse(exception.message)[field]?.toString()
+
+        private fun count(resultatKode: SPKResultatKodePre2025) {
+            Metrics.countTjenestepensjonSimuleringPre2025(resultatKode)
+        }
+
+        private fun ukjentFeil(exception: EgressException): Nothing {
+            count(UKJENT_FEIL_HOS_SPK)
+            throw exception
+        }
+
+        private fun filterFnr(s: String) = FNR_REGEX.replace(s, "*****")
 
             val MOCK_SPK_EXCEPTION1 = EgressException(
                 """{"errorCode":"CALC002","message":"Validation problem: Tjenestetid mindre enn 3 år."}""",
@@ -201,5 +237,5 @@ class TjenestepensjonSimuleringPre2025ForPensjonskalkulatorService(
                 brukerErIkkeMedlemAvTPOrdning = false,
                 brukerErMedlemAvTPOrdningSomIkkeStoettes = false
             )
-        }
     }
+}

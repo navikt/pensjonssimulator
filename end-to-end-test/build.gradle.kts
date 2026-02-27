1069329b01bd677eb3409e237be31cbec2c022d6
Espen Gjøstøl <50329374+espengjostol@users.noreply.github.com>
2026-02-27 09:46:34 +0100

    Bump Java from 21 to 25 (#393)
    Also:
    * Bump Gradle from 8.12 to 9.3.1
    * Update Gradle wrapper
    * Bump dependencies for end-to-end tests
    * Bump maven-surefire-plugin from 3.5.4 to 3.5.5

--- end-to-end-test/build.gradle.kts
+++ end-to-end-test/build.gradle.kts
@@ -1,15 +1,25 @@
-val kotestVersion = "5.7.2"
+val kotestVersion = "6.1.3"
 
 plugins {
-    kotlin("jvm") version "2.1.10"
-    id("io.ktor.plugin") version "3.1.2"
-    kotlin("plugin.serialization") version "2.1.10"
+    id("org.jetbrains.kotlin.jvm") version "2.3.10"
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.3.10"
+    id("io.ktor.plugin") version "3.4.0"
 }
 
 group = "no.nav.pensjon"
 
 application {
     mainClass = "no.nav.pensjon.IntegrasjonstestApplicationKt"
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(25))
+    }
+}
+
+kotlin {
+    jvmToolchain(25)
 }
 
 repositories {
@@ -27,24 +37,23 @@ tasks.withType<Test>().configureEach {
 }
 
 dependencies {
-    implementation("ch.qos.logback:logback-classic:1.4.14")
-    implementation("net.logstash.logback:logstash-logback-encoder:8.1")
+    implementation("ch.qos.logback:logback-classic:1.5.32")
+    implementation("net.logstash.logback:logstash-logback-encoder:9.0")
     implementation("io.github.microutils:kotlin-logging-jvm:3.0.5")
 
     implementation("io.ktor:ktor-client-cio")
     implementation("io.ktor:ktor-client-core")
     implementation("io.ktor:ktor-serialization-kotlinx-json")
     implementation("io.ktor:ktor-client-content-negotiation")
-    implementation("io.ktor:ktor-client-logging:3.1.3")
+    implementation("io.ktor:ktor-client-logging:3.4.0")
 
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.10.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
 
-    implementation("com.nimbusds:nimbus-jose-jwt:10.2")
-    implementation("com.typesafe:config:1.4.2")
+    implementation("com.nimbusds:nimbus-jose-jwt:10.8")
+    implementation("com.typesafe:config:1.4.3")
 
     testImplementation("io.kotest:kotest-runner-junit5-jvm:${kotestVersion}")
     testImplementation("io.kotest:kotest-assertions-core-jvm:${kotestVersion}")
     testImplementation("io.kotest:kotest-property-jvm:${kotestVersion}")
-
 }

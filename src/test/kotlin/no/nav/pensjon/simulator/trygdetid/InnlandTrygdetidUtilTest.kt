a81e5373557c27645dc0508eef429008f10c9571
Espen Gjøstøl <50329374+espengjostol@users.noreply.github.com>
2026-04-17 12:40:17 +0200

    TPP-270 Fix 1-day utenlandsopphold (#438)

--- src/test/kotlin/no/nav/pensjon/simulator/trygdetid/InnlandTrygdetidUtilTest.kt
+++ src/test/kotlin/no/nav/pensjon/simulator/trygdetid/InnlandTrygdetidUtilTest.kt
@@ -1,170 +1,228 @@
 package no.nav.pensjon.simulator.trygdetid
 
-import io.kotest.core.spec.style.FunSpec
+import io.kotest.core.spec.style.ShouldSpec
 import io.kotest.matchers.shouldBe
 import no.nav.pensjon.simulator.core.domain.regler.TTPeriode
 import no.nav.pensjon.simulator.core.domain.regler.enum.LandkodeEnum
+import no.nav.pensjon.simulator.core.util.toNorwegianDateAtNoon
 import no.nav.pensjon.simulator.testutil.TestDateUtil.atNoon
 import java.time.LocalDate
 
-class InnlandTrygdetidUtilTest : FunSpec({
+class InnlandTrygdetidUtilTest : ShouldSpec({
 
-    test("addInnenlandsopphold when closed utenlandsperiode: should add open norsk periode") {
-        val grunnlagListe: List<TrygdetidOpphold> =
-            InnlandTrygdetidUtil.addInnenlandsopphold(
-                oppholdListe = listOf(
-                    TrygdetidOpphold(
-                        periode = TTPeriode().apply {
-                            landEnum = LandkodeEnum.LTU
-                            fom = atNoon(1963, 1, 3)
-                            tom = atNoon(2027, 5, 1) // closed
-                        },
-                        arbeidet = false
-                    )
-                ),
-                foedselsdato = LocalDate.of(1963, 1, 3)
-            )
-
-        grunnlagListe.size shouldBe 2
-        with(grunnlagListe[0]) {
-            periode.landEnum shouldBe LandkodeEnum.LTU
-            periode.fom shouldBe atNoon(1963, 1, 3)
-            periode.tom shouldBe atNoon(2027, 5, 1)
-            arbeidet shouldBe false
-        }
-        with(grunnlagListe[1]) {
-            periode.landEnum shouldBe LandkodeEnum.NOR
-            periode.fom shouldBe atNoon(2027, 5, 2)
-            periode.tom shouldBe null
-            arbeidet shouldBe true
-        }
-    }
-
-    test("addInnenlandsopphold when open utenlandsperiode after fødselsdato: should add norsk periode between fødselsdato and utenlandsperiode") {
-        val grunnlagListe: List<TrygdetidOpphold> =
-            InnlandTrygdetidUtil.addInnenlandsopphold(
-                oppholdListe = listOf(
-                    TrygdetidOpphold(
-                        periode = TTPeriode().apply {
-                            landEnum = LandkodeEnum.LTU
-                            fom = atNoon(1971, 1, 1)
-                            tom = null // open
-                        },
-                        arbeidet = false
-                    )
-                ),
-                foedselsdato = LocalDate.of(1963, 2, 15)
-            )
-
-        grunnlagListe.size shouldBe 2
-        with(grunnlagListe[0]) {
-            periode.landEnum shouldBe LandkodeEnum.NOR
-            periode.fom shouldBe atNoon(1963, 2, 15)
-            periode.tom shouldBe atNoon(1970, 12, 31)
-            arbeidet shouldBe true
-        }
-        with(grunnlagListe[1]) {
-            periode.landEnum shouldBe LandkodeEnum.LTU
-            periode.fom shouldBe atNoon(1971, 1, 1)
-            periode.tom shouldBe null
-            arbeidet shouldBe false
-        }
-    }
-
-    test("addInnenlandsopphold handles unsorted open perioder") {
-        val grunnlagListe: List<TrygdetidOpphold> =
-            InnlandTrygdetidUtil.addInnenlandsopphold(
-                oppholdListe = listOf(
-                    TrygdetidOpphold(
-                        periode = TTPeriode().apply {
-                            landEnum = LandkodeEnum.FRA
-                            fom = atNoon(2024, 9, 4)
-                            tom = null // open
-                        },
-                        arbeidet = false
+    context("addInnenlandsopphold when closed utenlandsperiode") {
+        should("add open norsk periode") {
+            val grunnlagListe: List<TrygdetidOpphold> =
+                InnlandTrygdetidUtil.addInnenlandsopphold(
+                    oppholdListe = listOf(
+                        TrygdetidOpphold(
+                            periode = TTPeriode().apply {
+                                landEnum = LandkodeEnum.LTU
+                                fom = atNoon(1963, 1, 3)
+                                tom = atNoon(2027, 5, 1) // closed
+                            },
+                            arbeidet = false
+                        )
                     ),
-                    TrygdetidOpphold(
-                        periode = TTPeriode().apply {
-                            landEnum = LandkodeEnum.FRA
-                            fom = atNoon(2024, 6, 4) // before item above => unsorted
-                            tom = null // open
-                        },
-                        arbeidet = true
-                    )
-                ),
-                foedselsdato = LocalDate.of(1963, 2, 15)
-            )
+                    foedselsdato = LocalDate.of(1963, 1, 3)
+                )
 
-        grunnlagListe.size shouldBe 3
-        with(grunnlagListe[0]) {
-            periode.landEnum shouldBe LandkodeEnum.NOR
-            periode.fom shouldBe atNoon(1963, 2, 15)
-            periode.tom shouldBe atNoon(2024, 6, 3)
-            arbeidet shouldBe true
-        }
-        with(grunnlagListe[1]) {
-            periode.landEnum shouldBe LandkodeEnum.FRA
-            periode.fom shouldBe atNoon(2024, 6, 4)
-            periode.tom shouldBe null
-            arbeidet shouldBe true
-        }
-        with(grunnlagListe[2]) {
-            periode.landEnum shouldBe LandkodeEnum.FRA
-            periode.fom shouldBe atNoon(2024, 9, 4)
-            periode.tom shouldBe null
-            arbeidet shouldBe false
+            grunnlagListe.size shouldBe 2
+            with(grunnlagListe[0]) {
+                periode.landEnum shouldBe LandkodeEnum.LTU
+                periode.fom shouldBe atNoon(1963, 1, 3)
+                periode.tom shouldBe atNoon(2027, 5, 1)
+                arbeidet shouldBe false
+            }
+            with(grunnlagListe[1]) {
+                periode.landEnum shouldBe LandkodeEnum.NOR
+                periode.fom shouldBe atNoon(2027, 5, 2)
+                periode.tom shouldBe null
+                arbeidet shouldBe true
+            }
         }
     }
 
-    test("addInnenlandsopphold inserts missing perioder") {
-        val grunnlagListe: List<TrygdetidOpphold> =
-            InnlandTrygdetidUtil.addInnenlandsopphold(
-                oppholdListe = listOf(
-                    TrygdetidOpphold(
-                        periode = TTPeriode().apply {
-                            landEnum = LandkodeEnum.FRA
-                            fom = atNoon(2024, 6, 4)
-                            tom = atNoon(2024, 6, 30)
-                        },
-                        arbeidet = true
+    context("addInnenlandsopphold when open utenlandsperiode after fødselsdato") {
+        should("add norsk periode between fødselsdato and utenlandsperiode") {
+            val grunnlagListe: List<TrygdetidOpphold> =
+                InnlandTrygdetidUtil.addInnenlandsopphold(
+                    oppholdListe = listOf(
+                        TrygdetidOpphold(
+                            periode = TTPeriode().apply {
+                                landEnum = LandkodeEnum.LTU
+                                fom = atNoon(1971, 1, 1)
+                                tom = null // open
+                            },
+                            arbeidet = false
+                        )
                     ),
-                    // missing periode 2024-07-01 to 2024-09-03
-                    TrygdetidOpphold(
-                        periode = TTPeriode().apply {
-                            landEnum = LandkodeEnum.FRA
-                            fom = atNoon(2024, 9, 4)
-                            tom = null
-                        },
-                        arbeidet = false
-                    )
-                ),
-                foedselsdato = LocalDate.of(1963, 2, 15)
-            )
+                    foedselsdato = LocalDate.of(1963, 2, 15)
+                )
 
-        grunnlagListe.size shouldBe 4
-        with(grunnlagListe[0]) {
-            periode.landEnum shouldBe LandkodeEnum.NOR
-            periode.fom shouldBe atNoon(1963, 2, 15)
-            periode.tom shouldBe atNoon(2024, 6, 3)
-            arbeidet shouldBe true
+            grunnlagListe.size shouldBe 2
+            with(grunnlagListe[0]) {
+                periode.landEnum shouldBe LandkodeEnum.NOR
+                periode.fom shouldBe atNoon(1963, 2, 15)
+                periode.tom shouldBe atNoon(1970, 12, 31)
+                arbeidet shouldBe true
+            }
+            with(grunnlagListe[1]) {
+                periode.landEnum shouldBe LandkodeEnum.LTU
+                periode.fom shouldBe atNoon(1971, 1, 1)
+                periode.tom shouldBe null
+                arbeidet shouldBe false
+            }
         }
-        with(grunnlagListe[1]) {
-            periode.landEnum shouldBe LandkodeEnum.FRA
-            periode.fom shouldBe atNoon(2024, 6, 4)
-            periode.tom shouldBe atNoon(2024, 6, 30)
-            arbeidet shouldBe true
+    }
+
+    context("addInnenlandsopphold") {
+        should("handle unsorted open perioder") {
+            val grunnlagListe: List<TrygdetidOpphold> =
+                InnlandTrygdetidUtil.addInnenlandsopphold(
+                    oppholdListe = listOf(
+                        TrygdetidOpphold(
+                            periode = TTPeriode().apply {
+                                landEnum = LandkodeEnum.FRA
+                                fom = atNoon(2024, 9, 4)
+                                tom = null // open
+                            },
+                            arbeidet = false
+                        ),
+                        TrygdetidOpphold(
+                            periode = TTPeriode().apply {
+                                landEnum = LandkodeEnum.FRA
+                                fom = atNoon(2024, 6, 4) // before item above => unsorted
+                                tom = null // open
+                            },
+                            arbeidet = true
+                        )
+                    ),
+                    foedselsdato = LocalDate.of(1963, 2, 15)
+                )
+
+            grunnlagListe.size shouldBe 3
+            with(grunnlagListe[0]) {
+                periode.landEnum shouldBe LandkodeEnum.NOR
+                periode.fom shouldBe atNoon(1963, 2, 15)
+                periode.tom shouldBe atNoon(2024, 6, 3)
+                arbeidet shouldBe true
+            }
+            with(grunnlagListe[1]) {
+                periode.landEnum shouldBe LandkodeEnum.FRA
+                periode.fom shouldBe atNoon(2024, 6, 4)
+                periode.tom shouldBe null
+                arbeidet shouldBe true
+            }
+            with(grunnlagListe[2]) {
+                periode.landEnum shouldBe LandkodeEnum.FRA
+                periode.fom shouldBe atNoon(2024, 9, 4)
+                periode.tom shouldBe null
+                arbeidet shouldBe false
+            }
         }
-        with(grunnlagListe[2]) {
-            periode.landEnum shouldBe LandkodeEnum.NOR
-            periode.fom shouldBe atNoon(2024, 7, 1)
-            periode.tom shouldBe atNoon(2024, 9, 3)
-            arbeidet shouldBe true
+
+        should("insert missing perioder") {
+            val grunnlagListe: List<TrygdetidOpphold> =
+                InnlandTrygdetidUtil.addInnenlandsopphold(
+                    oppholdListe = listOf(
+                        TrygdetidOpphold(
+                            periode = TTPeriode().apply {
+                                landEnum = LandkodeEnum.FRA
+                                fom = atNoon(2024, 6, 4)
+                                tom = atNoon(2024, 6, 30)
+                            },
+                            arbeidet = true
+                        ),
+                        // missing periode 2024-07-01 to 2024-09-03
+                        TrygdetidOpphold(
+                            periode = TTPeriode().apply {
+                                landEnum = LandkodeEnum.FRA
+                                fom = atNoon(2024, 9, 4)
+                                tom = null
+                            },
+                            arbeidet = false
+                        )
+                    ),
+                    foedselsdato = LocalDate.of(1963, 2, 15)
+                )
+
+            grunnlagListe.size shouldBe 4
+            with(grunnlagListe[0]) {
+                periode.landEnum shouldBe LandkodeEnum.NOR
+                periode.fom shouldBe atNoon(1963, 2, 15)
+                periode.tom shouldBe atNoon(2024, 6, 3)
+                arbeidet shouldBe true
+            }
+            with(grunnlagListe[1]) {
+                periode.landEnum shouldBe LandkodeEnum.FRA
+                periode.fom shouldBe atNoon(2024, 6, 4)
+                periode.tom shouldBe atNoon(2024, 6, 30)
+                arbeidet shouldBe true
+            }
+            with(grunnlagListe[2]) {
+                periode.landEnum shouldBe LandkodeEnum.NOR
+                periode.fom shouldBe atNoon(2024, 7, 1)
+                periode.tom shouldBe atNoon(2024, 9, 3)
+                arbeidet shouldBe true
+            }
+            with(grunnlagListe[3]) {
+                periode.landEnum shouldBe LandkodeEnum.FRA
+                periode.fom shouldBe atNoon(2024, 9, 4)
+                periode.tom shouldBe null
+                arbeidet shouldBe false
+            }
         }
-        with(grunnlagListe[3]) {
-            periode.landEnum shouldBe LandkodeEnum.FRA
-            periode.fom shouldBe atNoon(2024, 9, 4)
-            periode.tom shouldBe null
-            arbeidet shouldBe false
+
+        should("handle 1-dags utenlandsopphold") {
+            val grunnlagListe: List<TrygdetidOpphold> =
+                InnlandTrygdetidUtil.addInnenlandsopphold(
+                    oppholdListe = listOf(
+                        TrygdetidOpphold(
+                            periode = TTPeriode().apply {
+                                landEnum = LandkodeEnum.DZA
+                                fom = LocalDate.of(2026, 4, 27).toNorwegianDateAtNoon()
+                                tom = LocalDate.of(2026, 4, 30).toNorwegianDateAtNoon()
+                            },
+                            arbeidet = true
+                        ),
+                        TrygdetidOpphold(
+                            periode = TTPeriode().apply {
+                                landEnum = LandkodeEnum.AND
+                                fom = LocalDate.of(2026, 4, 30).toNorwegianDateAtNoon()
+                                tom = LocalDate.of(2026, 4, 30).toNorwegianDateAtNoon()
+                            },
+                            arbeidet = false
+                        )
+                    ),
+                    foedselsdato = LocalDate.of(1963, 2, 15)
+                )
+
+            grunnlagListe.size shouldBe 4
+            with(grunnlagListe[0]) {
+                periode.landEnum shouldBe LandkodeEnum.NOR
+                periode.fom shouldBe LocalDate.of(1963, 2, 15).toNorwegianDateAtNoon()
+                periode.tom shouldBe LocalDate.of(2026, 4, 26).toNorwegianDateAtNoon()
+                arbeidet shouldBe true
+            }
+            with(grunnlagListe[1]) {
+                periode.landEnum shouldBe LandkodeEnum.DZA
+                periode.fom shouldBe LocalDate.of(2026, 4, 27).toNorwegianDateAtNoon()
+                periode.tom shouldBe LocalDate.of(2026, 4, 30).toNorwegianDateAtNoon()
+                arbeidet shouldBe true
+            }
+            with(grunnlagListe[2]) {
+                periode.landEnum shouldBe LandkodeEnum.AND
+                periode.fom shouldBe LocalDate.of(2026, 4, 30).toNorwegianDateAtNoon()
+                periode.tom shouldBe LocalDate.of(2026, 4, 30).toNorwegianDateAtNoon()
+                arbeidet shouldBe false
+            }
+            with(grunnlagListe[3]) {
+                periode.landEnum shouldBe LandkodeEnum.NOR
+                periode.fom shouldBe LocalDate.of(2026, 5, 1).toNorwegianDateAtNoon()
+                periode.tom shouldBe null
+                arbeidet shouldBe true
+            }
         }
     }
 })

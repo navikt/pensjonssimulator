cdf02e764cc0796a18145715caf36455738c4cf1
Espen Gjøstøl <espen.oskar.gjostol@nav.no>
2026-05-08 16:53:06 +0200

    TPP-276 Include 'endring av AP'

--- src/main/kotlin/no/nav/pensjon/simulator/core/ufoere/UfoereperiodeService.kt
+++ src/main/kotlin/no/nav/pensjon/simulator/core/ufoere/UfoereperiodeService.kt
@@ -1,8 +1,6 @@
 package no.nav.pensjon.simulator.core.ufoere
 
-import no.nav.pensjon.simulator.core.domain.regler.enum.SimuleringTypeEnum
 import no.nav.pensjon.simulator.core.domain.regler.grunnlag.Persongrunnlag
-import no.nav.pensjon.simulator.core.krav.UttakGradKode
 import no.nav.pensjon.simulator.core.spec.SimuleringSpec
 import no.nav.pensjon.simulator.normalder.NormertPensjonsalderService
 import org.springframework.stereotype.Service
@@ -22,22 +20,9 @@ class UfoereperiodeService(
         val uttakFom: LocalDate = spec.foersteUttakDato!!
         val tidligUttak: Boolean = uttakFom.isBefore(dagenFoerUbetingetUttaksdato)
 
-        return if (tidligUttak && skalTerminereUfoereperiodeVedUttaksdato(spec))
+        return if (tidligUttak && spec.kreverAvsluttetUfoeretrygd)
             uttakFom.minusDays(1)
         else
             dagenFoerUbetingetUttaksdato
     }
-
-    /**
-     * Uføretrygd må avsluttes før uttak av pensjon i følgende tilfeller:
-     * - Ved uttak (helt eller gradert) av alderspensjon i kombinasjon med privat AFP
-     * - Ved helt uttak av alderspensjon
-     */
-    private fun skalTerminereUfoereperiodeVedUttaksdato(spec: SimuleringSpec): Boolean =
-        spec.type == SimuleringTypeEnum.ALDER_M_AFP_PRIVAT ||
-                setOf(
-                    SimuleringTypeEnum.ALDER,
-                    SimuleringTypeEnum.ALDER_M_GJEN
-                ).contains(spec.type) && spec.uttakGrad == UttakGradKode.P_100
-
 }

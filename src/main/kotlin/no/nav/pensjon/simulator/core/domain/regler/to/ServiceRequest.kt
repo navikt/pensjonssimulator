c85c9cae8b494aefacc77d98a5e7d37049cbffdc
Espen Gjøstøl <50329374+espengjostol@users.noreply.github.com>
2026-06-09 10:55:16 +0200

    Update e2e test responses for new satstabell (#484)

--- src/main/kotlin/no/nav/pensjon/simulator/core/domain/regler/to/ServiceRequest.kt
+++ src/main/kotlin/no/nav/pensjon/simulator/core/domain/regler/to/ServiceRequest.kt
@@ -1,11 +1,12 @@
 package no.nav.pensjon.simulator.core.domain.regler.to
 
-// 2026-04-23 + default satstabell
+// Copied from pensjon-regler-api v2.0.0 2026-06-08
+// plus given value for satstabell to make end-to-end tests stable
 abstract class ServiceRequest {
 
     /**
      * Satsstabell som skal benyttes ved beregning.
      * Kun lest av pensjon-regler i test miljø.
      */
-    var satstabell: String? = "PROD_20250701"
+    var satstabell: String? = "PROD_20260701"
 }

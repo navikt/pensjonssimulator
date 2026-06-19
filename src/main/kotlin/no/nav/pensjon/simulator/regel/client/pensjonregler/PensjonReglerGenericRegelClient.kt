a3a6ea3ff77872a0fc638008200c5186c6373781
Espen Gjøstøl <espen.oskar.gjostol@nav.no>
2026-06-19 15:20:30 +0200

    TPP-500 Use consumer ID in regler calls
    Also remove unused function arguments

--- src/main/kotlin/no/nav/pensjon/simulator/regel/client/pensjonregler/PensjonReglerGenericRegelClient.kt
+++ src/main/kotlin/no/nav/pensjon/simulator/regel/client/pensjonregler/PensjonReglerGenericRegelClient.kt
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType
 import org.springframework.stereotype.Component
+import org.springframework.web.reactive.function.client.bodyToMono
 import tools.jackson.databind.json.JsonMapper
 import java.util.Objects.requireNonNull
 
@@ -25,28 +26,22 @@ class PensjonReglerGenericRegelClient(
 ) : ExternalServiceClient(retryAttempts), GenericRegelClient {
     private val webClient = webClientBase.withBaseUrl(baseUrl)
 
-    // regelServiceApi
     override fun <K, T : Any> makeRegelCall(
         request: T,
         responseClass: Class<*>,
-        serviceName: String,
-        map: Map<String, Any>?,
-        sakId: String?
+        serviceName: String
     ): K {
         return try {
-            callPensjonRegler(serviceName, request, responseClass, map, sakId)
+            callPensjonRegler(serviceName, request, responseClass)
         } catch (e: Exception) {
             throw ImplementationUnrecoverableException("Request failed to pensjon-regler", e)
         }
     }
 
-    // pensjonReglerRest
     private fun <T> callPensjonRegler(
         serviceName: String,
         request: Any,
-        responseClass: Class<*>,
-        extra: Map<String, Any>?,
-        sakId: String?
+        responseClass: Class<*>
     ): T {
         val uri = "$BASE_PATH/$serviceName"
 
@@ -58,18 +53,18 @@ class PensjonReglerGenericRegelClient(
             .headers(::setHeaders)
             .bodyValue(request)
             .retrieve()
-            .bodyToMono(String::class.java)
+            .bodyToMono<String>()
             .retryWhen(retryBackoffSpec(uri))
             .block()
 
         return requireNonNull(objectMapper.readValue(requireNonNull(responseBody), responseClass) as T)
     }
 
-    // makeHttpHeaders
     private fun setHeaders(headers: HttpHeaders, sakId: String? = null) {
         sakId?.let { headers["sakId"] = it }
         headers.contentType = MediaType.APPLICATION_JSON
         headers[CustomHttpHeaders.CALL_ID] = traceAid.callId()
+        headers[CustomHttpHeaders.CONSUMER_ID] = "pensjonssimulator"
     }
 
     override fun toString(e: EgressException, uri: String) = "Failed calling $uri"

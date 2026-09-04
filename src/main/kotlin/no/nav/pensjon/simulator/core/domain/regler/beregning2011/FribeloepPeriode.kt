d9c5d15cd7fa63dc0a88ac831c397b6c6cdcc07f
Espen Gjøstøl <espen.oskar.gjostol@nav.no>
2026-09-04 10:42:03 +0200

    TPP-598 Oppdater i.h.t. pensjon-regler-api 2.4.3

---
+++ src/main/kotlin/no/nav/pensjon/simulator/core/domain/regler/beregning2011/FribeloepPeriode.kt
@@ -0,0 +1,38 @@
+package no.nav.pensjon.simulator.core.domain.regler.beregning2011
+
+import no.nav.pensjon.simulator.core.domain.regler.enum.VenteperiodeBegrunnelseEnum
+import java.time.LocalDate
+
+// Copied from pensjon-regler-api v2.4.2 2026-09-04
+// NB: Name is 'FribeløpPeriode' in pensjon-regler-api
+class FribeloepPeriode(
+    /**
+     * Fra hvilken måned i virkåret perioden gjelder fra og med.
+     */
+    val fom: LocalDate,
+
+    /**
+     * Fra hvilken måned i virkåret perioden gjelder til og med.
+     */
+    var tom: LocalDate,
+
+    /**
+     * Hvilken faktor perioden bidrar med ved beregning av fribeløp.
+     */
+    val faktor: Double,
+
+    /**
+     * Grunnbeløpet som legges til grunn for perioden.
+     */
+    val grunnbeløp: Int,
+
+    /**
+     * Fra hvilken dato venteperioden har startet å telle fra.
+     */
+    val venteperiodeStartDato: LocalDate,
+
+    /**
+     * Årsaken til hvorfor venteperioden har startet.
+     */
+    val venteperiodeBegrunnelseEnum: VenteperiodeBegrunnelseEnum
+)
